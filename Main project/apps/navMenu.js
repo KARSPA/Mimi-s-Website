ab54af58c58551f0f8f1e4adc5ba6dbb8a0357f1
KARSPA <kaspar.ivan@outlook.com>
2024-05-01 12:50:12 +0200

    Ajout du menu rideau et style rresponsive/annulation du scroll et modification sur toutes les pages

--- Main project/apps/navMenu.js
+++ Main project/apps/navMenu.js
@@ -4,9 +4,28 @@ const hamburgerBtn = document.querySelector(".menu-icon-btn")
 
 const navigation = document.querySelector(".navbar ")
 
-hamburgerBtn.addEventListener("click", toggleNav)
+hamburgerBtn.addEventListener("click", toggleNavAndScroll)
 
-function toggleNav(){
+function toggleNavAndScroll(){
     hamburgerBtn.classList.toggle("active")
     navigation.classList.toggle("active")
+    console.log(document.body)
+    console.log(window.scrollY)
+    
+    if(navigation.classList.contains("active")){
+        
+            document.body.style.top = `-${window.scrollY}px`;
+            // console.log("Oui : " + document.body.style.top)
+            document.body.style.position = 'fixed';
+
+        
+    }else{
+        const scrollY = document.body.style.top;
+        document.body.style.position = '';
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
+    }
 }
+
+// window.addEventListener("scroll", () => console.log(window.scrollY))
